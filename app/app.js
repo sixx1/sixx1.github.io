e96e3359571039322b41f7a0d9434061daa64643
sixx1 <stefansixx@gmail.com>
2016-04-05 14:27:08 +0200

    tzpo

--- app/app.js
+++ app/app.js
@@ -82,7 +82,7 @@ mainApp.controller('projectYear', function($scope, $http, $routeParams, $locatio
 });
 
 mainApp.controller('projectB', function($scope, $http, $routeParams) {
-	$http.get('appapp/project.json').success(function(data) {
+	$http.get('app/project.json').success(function(data) {
 		for(var i = 0; i<data.length; i++) {
 			if (data[i].id === parseInt($routeParams.id)) {
 				return $scope.item = data[i];			
